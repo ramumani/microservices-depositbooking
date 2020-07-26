68dd9fabde7f3fd37cf64fc55e8b5115975b3c75
ramu.mani@hotmail.com <ramu.mani@hotmail.com>
2020-07-26 11:00:29 +0300

    First commit.

---
+++ discovery-server/src/test/java/com/ramumani/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramumani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
