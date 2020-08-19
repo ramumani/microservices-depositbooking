3d024fcea0632a83e90ce03eeb98600c6774407c
ramu.mani@hotmail.com <ramu.mani@hotmail.com>
2020-08-19 09:52:08 +0300

    updating config.

--- interest-rate-service/src/main/java/com/ramumani/InterestRateServiceApplication.java
+++ interest-rate-service/src/main/java/com/ramumani/InterestRateServiceApplication.java
@@ -2,11 +2,11 @@ package com.ramumani;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class InterestRateServiceApplication {
 
 	public static void main(String[] args) {
