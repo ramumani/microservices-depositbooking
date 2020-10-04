0ac72fb487b5941270197f2cfb7751c6c60c0f6c
ramumani <ramu.mani@hotmail.com>
2020-10-04 12:53:55 +0300

    Enabling Eureka client.

--- fixed-deposit-service/src/main/java/com/ramumani/FixedDepositServiceApplication.java
+++ fixed-deposit-service/src/main/java/com/ramumani/FixedDepositServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class FixedDepositServiceApplication {
 	
 	@Bean
